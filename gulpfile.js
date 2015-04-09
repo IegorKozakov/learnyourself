e4389cf7d101ae46075204517ed2d0d1ac0d6613
dimaspirit <dimaspirit@inbox.ru>
2015-04-09 19:47:17 +0300

    - add gulp task for css - some css file - it's for testing NEED - add others gulp plugin

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,17 @@
 var gulp = require('gulp'),
+    concatCss = require('gulp-concat-css'),
+    minifyCss  = require('gulp-minify-css'),
+    rename = require('gulp-minify-css')
     connect = require('gulp-connect');
+
+
+/* for development */
+gulp.task('devConcatCss', function() {
+    return gulp.src('src/**/*.css')
+        .pipe(concatCss('styles.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dest/'));
+});
 
 gulp.task('connect', function() {
   connect.server({
@@ -7,4 +19,18 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['connect']);
+/* for production */
+gulp.task('buildConcatCss', function() {
+    return gulp.src('src/**/*.css')
+        .pipe(concatCss('styles.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dest/'));
+});
+
+
+gulp.task('watch', function() {
+    gulp.watch('src/**/*.css', ['devConcatCss'])
+});
+
+/* DEFAULT task only for development */
+gulp.task('default', ['connect', 'devConcatCss']);
