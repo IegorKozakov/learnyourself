2e86f399ddc2204955cf24a14d52eae6564a120b
dimaspirit <dimaspirit@inbox.ru>
2015-04-28 20:02:22 +0300

    gulp  - add livereload  - minificate structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,65 @@
-var gulp = require('gulp'),
-    concatCss = require('gulp-concat-css'),
-    connect = require('gulp-connect');
+/**
+ * For livereload read
+ * [https://scotch.io/tutorials/a-quick-guide-to-using-livereload-with-gulp]
+ */
 
-gulp.task('developCSS', function() {
+var gulp         = require('gulp'),
+    concatCss    = require('gulp-concat-css'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifyCss    = require('gulp-minify-css'),
+    rename       = require("gulp-rename"),
+    uglify       = require('gulp-uglify'),
+    concat       = require('gulp-concat'),
+    livereload   = require('gulp-livereload');
+
+var path = {
+    bower: 'bower_components/',
+    css : {
+        src: 'src/css/',
+        dest: 'dest/css/'
+    },
+    js : {
+        dest: 'dest/js'
+    }
+}
+
+gulp.task('styles', function() {
     return gulp.src([
-            'src/css/base/reset.css',
-            'src/css/base/helpers.css',
-            'src/css/common.css',
-            'src/css/features/*.css'
-        ])
-        .pipe(concatCss('styles.css'))
-        .pipe(gulp.dest('dest/css/'));
+        path.css.src + 'common.css',
+        path.css.src + 'features/*.css'
+    ])
+    .pipe(concatCss('styles.css'))
+    .pipe(autoprefixer({
+        browsers: [
+            'last 4 version',
+            'FireFox 28',
+            'ie 9',
+            'Safari 5'
+        ],
+        cascade: false
+    }))
+    .pipe(gulp.dest(path.css.dest))
+    .pipe(livereload());;
 });
 
-gulp.task('watchCSS', function() {
-    gulp.watch('src/css/**/*', ['developCSS']);
+gulp.task('concat-js-libs', function() {
+    return gulp.src([
+        path.bower + 'jquery/dist/jquery.js',
+        path.bower + 'underscore/underscore.js',
+        path.bower + 'backbone/backbone.js',
+        path.bower + 'handlebars/handlebars.js'
+    ])
+    .pipe(concat('libs.js'))
+    .pipe(uglify())
+    .pipe(rename({
+        suffix: '.min'
+    }))
+    .pipe(gulp.dest(path.js.dest));
 });
 
+gulp.task('watch', function() {
+    livereload.listen({ start: true });
+    gulp.watch(path.css.src + '**/*.css', ['styles']);
+});
 
-/* DEFAULT task for development */
-gulp.task('dev', ['developCSS', 'watchCSS']);
+gulp.task('default', ['watch', 'styles']);
--- src/js/helpers.js
+++ src/js/helpers.js
@@ -2,20 +2,51 @@
  * helpers.js
  *  - getTpl
  */
-
 ;
 (function(window, $, Handlebars){
-  'use strict';
+    'use strict';
 
-  var PATH_TO_STATIC_PAGE = 'static_page/';
+    window.LY = window.LY || {};
+    window.LY.version = '0.0.1';
 
-  LY.namespace('Helpers');
+    $(function(){
+        new LY.Router();
+        Backbone.history.start();
+    });
 
-  /**
-   * [getTpl get template by id]
-   * @param  {[string]} id [id of tag]
-   * @return {[html]}      [compiles html of template]
-   */
+    /** XXX - don't know - main.js or helpers.js
+    * [namespace - create new namespace]
+    * @param  {[string]} newNamespace [name of new namespace]
+    * @return {[object]}              [root namespace object]
+    */
+    LY.namespace = function (newNamespace) {
+        var namespaces =  newNamespace.split('.'),
+            parent = LY,
+            i, j;
+
+        if(namespaces[0] === 'LY') {
+            namespaces = namespaces.slice(1);
+        }
+
+        for (i = 0, j = namespaces.length; i < j; i++) {
+            if (typeof parent[namespaces[i]] === 'undefined' ) {
+             parent[namespaces[i]] = {};
+            }
+            parent = parent[namespaces[i]];
+        }
+
+        return parent;
+    };
+
+    var PATH_TO_STATIC_PAGE = 'static_page/';
+
+    LY.namespace('Helpers');
+
+    /**
+    * [getTpl get template by id]
+    * @param  {[string]} id [id of tag]
+    * @return {[html]}      [compiles html of template]
+    */
     LY.Helpers.getTpl = function(id) {
         return Handlebars.compile( $('#' + id).html() );
     };
@@ -48,9 +79,9 @@
      * @return {[string]}      [transformed word]
      */
     Handlebars.registerHelper('declOfNum', function(n, t) {
-      var cases = [2, 0, 1, 1, 1, 2],
-          titles = t.split('/');
+        var cases = [2, 0, 1, 1, 1, 2],
+        titles = t.split('/');
 
-      return titles[ (n%100>4 && n%100<20) ? 2 : cases[(n%10<5) ? n%10:5] ];
+        return titles[ (n%100>4 && n%100<20) ? 2 : cases[(n%10<5) ? n%10:5] ];
     });
 }(window, jQuery, Handlebars));
