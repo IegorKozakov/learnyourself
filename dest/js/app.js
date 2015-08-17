3449a29236d37091db32c8d0746afde7ff6c41fa
dimaspirit <dimaspirit@inbox.ru>
2015-08-17 18:15:25 +0300

    add comparator for collection

--- dest/js/app.js
+++ dest/js/app.js
@@ -427,7 +427,10 @@
     LY.namespace('Collections');
 
     LY.Collections.Courses = Backbone.Collection.extend({
-        model: LY.Models.Course
+        model: LY.Models.Course,
+        comparator: function(i) {
+            return !i.get('starred');
+        }
     });
 }(window, jQuery, _, Backbone));
 ;
